d88e46b1595197bbadc2cc36eb1996a0b32c37c7
VanesaMeze <mezovanessza39@gmail.com>
2023-02-28 16:40:01 +0100

    Added comments

--- single-post.php
+++ single-post.php
@@ -19,6 +19,52 @@
     <link href="styles/styles.css" rel="stylesheet">
 </head>
 
+<?php include('header.php');
+    if (isset($_GET['post_id'])) {
+        $sql = "SELECT author.*, posts.* 
+        FROM posts INNER JOIN author ON author.id = posts.author_id 
+        WHERE posts.id = {$_GET['post_id']}";
 
-<?php include "sidebar.php"; ?>
+        $statement = $connection->prepare($sql);
+        $statement->execute();
+        $statement->setFetchMode(PDO::FETCH_ASSOC);
+        $singlePost = $statement->fetch();
+
+        $sql2 = "SELECT * FROM comments WHERE post_id = {$_GET['post_id']}";
+        $statement = $connection->prepare($sql2);
+        $statement->execute();
+        $statement->setFetchMode(PDO::FETCH_ASSOC);
+        $comments = $statement->fetchAll();
+
+    }
+?>
+
+
+<main role="main" class="container">
+    <div class="row">
+        <div class="col-sm-8 blog-main">
+            <div class="blog-post">
+                    <article class="va-c-article">
+                        <form action="single_post.php" method="post">
+                            <input hidden type="text" name="post_id" value="<?php echo($singlePost['id']) ?>">
+                            <br><br>
+                        </form> 
+                        <header>
+                            <h1><?php echo $singlePost['title'] ?></h1>
+                            <div class="va-c-article__meta"><?php echo $singlePost['created_at']?> by <?php echo $singlePost['first_name'] . " " . $singlePost['last_name']?></div>
+                        </header>
+
+                        <div>
+                            <p><?php echo $singlePost['body']?></p>
+                        </div>
+                        <?php include('comments.php'); ?>
+                        </div>
+                    </article>
+            </div>
+        </div>
+        <?php include "sidebar.php"; ?>
+    </div>
+</main>
 <?php include "footer.php"?>
+</html>
+
